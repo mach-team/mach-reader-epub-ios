dfb6a258df8af68f6ae8d4c68c32af6b17024910
ngo275 <ngo38114@gmail.com>
2018-05-08 22:39:21 +0900

    modified reader config for sample

--- MachReader/ViewController.swift
+++ MachReader/ViewController.swift
@@ -23,10 +23,31 @@ class ViewController: UIViewController {
     }
 
     func open() {
-        let config = FolioReaderConfig()
+        //let config = FolioReaderConfig()
+        let config = readerConfiguration()
         let bookPath = Bundle.main.path(forResource: "book", ofType: "epub")
         let folioReader = FolioReader()
         folioReader.presentReader(parentViewController: self, withEpubPath: bookPath!, andConfig: config)
+    }
+    
+    private func readerConfiguration() -> FolioReaderConfig {
+        let config = FolioReaderConfig(withIdentifier: "SAMPLE")
+        config.shouldHideNavigationOnTap = false
+        config.scrollDirection = .vertical
+
+        // Custom sharing quote background
+        config.quoteCustomBackgrounds = []
+        if let image = UIImage(named: "demo-bg") {
+            let customImageQuote = QuoteImage(withImage: image, alpha: 0.6, backgroundColor: UIColor.black)
+            config.quoteCustomBackgrounds.append(customImageQuote)
+        }
+
+        let textColor = UIColor(red:0.86, green:0.73, blue:0.70, alpha:1.0)
+        let customColor = UIColor(red:0.30, green:0.26, blue:0.20, alpha:1.0)
+        let customQuote = QuoteImage(withColor: customColor, alpha: 1.0, textColor: textColor)
+        config.quoteCustomBackgrounds.append(customQuote)
+
+        return config
     }
 }
 
